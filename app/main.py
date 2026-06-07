ff30424baf43c79c0de2c5f72b18e69586d1aae4
Coderklng <kkartiksharma999@gmail.com>
2026-06-07 23:02:31 +0530

    changes main.py file

--- app/main.py
+++ app/main.py
@@ -100,4 +100,4 @@ def download_file(file_id: str):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000)) # Render PORT variable dega, nahi toh default 10000
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
