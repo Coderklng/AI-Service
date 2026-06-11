b05adf5d5c5d64d8ecc73b34fd748e4df4092ae2
Coderklng <kkartiksharma999@gmail.com>
2026-06-11 15:48:37 +0530

    fix: updated vector db path to /tmp and added safety checks for embedding

--- app/vectorstores/faiss_store.py
+++ app/vectorstores/faiss_store.py
@@ -1,21 +1,24 @@
 from langchain_community.vectorstores import FAISS 
+import os
 
+# Folder path define karo
+PERSISTENT_DIR = "/tmp/vector_db"
+
+# Ye ensure karega ki agar folder nahi hai, toh ban jaye
+os.makedirs(PERSISTENT_DIR, exist_ok=True)
 class VectorDB:
-    def __init__(self,embedding):
-        self.embedding = embedding 
+    def __init__(self, embedding):
+        self.embedding = embedding
+      # Path fix kar diya
         
-    def storeDb(self,docs):
-        db = FAISS.from_documents(
-            docs,
-            self.embedding
-        )
-        db.save_local("vector_db")
+    def storeDb(self, docs):
+        db = FAISS.from_documents(docs, self.embedding)
+        db.save_local(PERSISTENT_DIR)
         return db
     
-
     def localDb(self):
         return FAISS.load_local(
-            "vector_db",
+            PERSISTENT_DIR,
             self.embedding,
             allow_dangerous_deserialization=True
         )
